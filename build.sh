c0373f901ad2cd5bd8d0e815b58851886ba5ccc7
Kurre <baijeri@gmail.com>
2013-11-21 20:52:02 +0200

    Script: Add build script

---
+++ build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Stupid shell script to compile kernel, nothing fancy
+# Original script from SlimBean Mako kernel
+
+((!$#)) && echo -e "No arguments supplied!\nTry 'config', 'menuconfig' or value of '1-9'" && exit 1
+
+
+VERSION=`cat .version`
+NEWVERSION=$(expr $VERSION + 1)
+
+# Exports all the needed things Arch, SubArch and Cross Compile
+export ARCH=arm
+echo 'exporting Arch'
+export SUBARCH=arm
+echo 'exporting SubArch'
+
+##GCC 4.8
+#export CROSS_COMPILE=/media/dev/android-ndk-r9/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
+
+##GCC 4.7
+export CROSS_COMPILE=/media/geek/dev-disk/kernel/toolchains/arm-linux-androideabi-4.7/bin/arm-linux-androideabi-
+
+##Linaro 4.7 2013.04
+#export CROSS_COMPILE=/home/geek/dev-disk/kernel/toolchains/gcc-linaro-4.7-2013.04/bin/arm-linux-gnueabihf-
+
+##Linaro 4.8
+#export CROSS_COMPILE=/home/geek/dev-disk/kernel/toolchains/gcc-linaro-4.8-2013.08/bin/arm-linux-gnueabihf-
+
+echo 'exporting Cross Compile'
+
+# Generates a new .config and exists
+if [ "$1" = "config" ] ; then
+	echo 'Making defconfig for Manta'
+	make slim_manta_defconfig
+	exit
+fi
+
+# Generates a new .config and exists
+if [ "$1" = "menuconfig" ] ; then
+	echo 'Making defconfig for Manta and launching menuconfig'
+	make slim_manta_defconfig
+	make menuconfig
+	cp .config arch/arm/configs/slim_manta_defconfig
+	exit
+fi
+
+# Exports kernel local version? Not sure yet.
+#echo 'Exporting kernel version'
+#export LOCALVERSION='SlimTest_1.0'
+
+#Let's check if $1 is a number
+re='^[0-9]+$'
+if [[ $1 =~ $re ]] ; then
+	# Lets go!
+	echo 'Lets start!'
+
+	# Make sure build is clean!
+	echo "Removing old zImage"
+	rm -f arch/arm/boot/zImage
+	echo 'Cleaning build'
+	make clean
+
+	#make -j$1
+	make -j$1 V=99 2>&1 |tee build-r$NEWVERSION.log
+else
+	echo "Insert proper argument!"
+fi
